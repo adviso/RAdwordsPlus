e20b6e3fe139a03888a9eeb6606a9233da8f0abe
nicolas-scott <nicolas.scott@umontreal.ca>
2017-03-31 11:05:56 -0400

    Minor documentation fix

--- R/statement.R
+++ R/statement.R
@@ -5,7 +5,7 @@
 #' @param report Name of the report. Default to the account performance report ("ACCOUNT_PERFORMANCE_REPORT").
 #' @param fields Fields to select in the report. This can be a mix of attributes, segments and metrics. Default to "AccountDescriptiveName", "Impressions", "Clicks", "Cost", "Date".
 #' @param date Either a date range (see \url{https://developers.google.com/adwords/api/docs/guides/reporting#date-ranges}) or a vector of length two with the date interval (start date first, end date last). Default to last fourteen days ("LAST_14_DAYS")
-#' @param where Conditions to use in the statement, like restrictions on a metric
+#' @param where Conditions to use in the statement, like restrictions on a field
 #'
 #' @return A character string with the content of the query.
 #' @export
