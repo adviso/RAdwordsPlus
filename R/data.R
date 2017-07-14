40d0be7468c4de58082c63a32d85dcfe58120443
nicolas-scott <nicolas.scott@umontreal.ca>
2017-07-14 11:29:01 -0400

    Documentation for the adwords.* dataset

--- R/data.R
+++ R/data.R
@@ -1,6 +1,7 @@
 #' Adwords Reports
 #'
-#' A table containing all the Adwords reports for the 201609, 201607 and 201605 API versions.
+#' A table containing all the Adwords reports for the 201702, 201609 and 201607 API versions.
+#' This data is extracted from the files in the RAdwords package.
 #'
 #' @format A data frame with 132 rows and 3 variables
 #' @keywords datasets Adwords Report
@@ -8,7 +9,8 @@
 
 #' Adwords Report Fields
 #'
-#' A table containing all the Adwords report fields (attributes, segments and metrics) for the 201609, 201607 and 201605 API versions.
+#' A table containing all the Adwords report fields (attributes, segments and metrics) for the 201702, 201609 and 201607 API versions.
+#' This data is extracted from the files in the RAdwords package.
 #'
 #' @format A data frame with 7830 rows and 11 variables
 #' @keywords datasets Adwords Field Attribute Segment Metric
