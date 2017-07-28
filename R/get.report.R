22874ca868ddcec017e448391469cddc40d16a82
nicolas-scott <nicolas.scott@umontreal.ca>
2017-07-28 13:44:21 -0400

    Standardize get.report arguments to go along get.service format

--- R/get.report.R
+++ R/get.report.R
@@ -3,9 +3,9 @@
 #' Downloads an Adwords report. Wrapper function around the \link[RAdwords]{statement} and \link[RAdwords]{getData} functions.
 #' Allows for some additional feature with the dates.
 #'
-#' @param cid Customer client ids (or vector of).
+#' @param report Name of the report. The list of reports can be found at \url{https://developers.google.com/adwords/api/docs/appendix/reports}.
+#' @param cid Customer client ids (or vector of). If more than one CID is provided, the results will be concatenated.
 #' @param auth Google AdWords authentification token.
-#' @param name Name of the report. Default to the account performance report ("ACCOUNT_PERFORMANCE_REPORT").
 #' @param fields Metrics to select in the report. This can be a mix of attributes, segments and metrics. Default to "AccountDescriptiveName", "Impressions", "Clicks", "Cost", "Date".
 #' @param date Either a date range (see \url{https://developers.google.com/adwords/api/docs/guides/reporting#date-ranges}) or a vector of length two with the date interval (start date first, end date last). Default to last fourteen days ("LAST_14_DAYS"). If NULL the during clause will be omitted, getting data for the whole time. Note that this works only if no Date or Week columns are specified (see \url{https://developers.google.com/adwords/api/docs/guides/awql}).
 #' @param ... Extra parameters for the RAdwords::statement function
@@ -16,18 +16,19 @@
 #' @importFrom data.table rbindlist
 #'
 #' @examples
-#' # Download the search query performance report for January 2015
-#' auth <- doAuth()
-#' name <- "SEARCH_QUERY_PERFORMANCE_REPORT"
-#' fields <- c("AccountDescriptiveName", "AdGroupId", "AdGroupName", "AdGroupStatus", "CampaignId", "CampaignName", "CampaignStatus", "KeywordId", "KeywordTextMatchingQuery", "QueryMatchTypeWithVariant", "Impressions", "Clicks", "Conversions", "Cost", "AveragePosition")
+#' # Download the search query performance report for the last 14 days.
+#' library(RAdwords)
+#' report <- "SEARCH_QUERY_PERFORMANCE_REPORT"
 #' cid <- c('XXX-XXX-XXXX', 'YYY-YYY-YYYY')
-#' report <- get.report(cid, auth, name, fields, date = "LAST_14_DAYS")
-get.report <- function(cid, auth, name = "ACCOUNT_PERFORMANCE_REPORT", fields = c("AccountDescriptiveName", "Impressions", "Clicks", "Cost", "Date"), date = "LAST_14_DAYS", ...)
+#' auth <- doAuth()
+#' fields <- c("AccountDescriptiveName", "AdGroupId", "AdGroupName", "AdGroupStatus", "CampaignId", "CampaignName", "CampaignStatus", "KeywordId", "KeywordTextMatchingQuery", "QueryMatchTypeWithVariant", "Impressions", "Clicks", "Conversions", "Cost", "AveragePosition")
+#' report <- get.report(report, cid, auth, fields, date = "LAST_14_DAYS")
+get.report <- function(report, cid, auth, fields = c("AccountDescriptiveName", "Impressions", "Clicks", "Cost", "Date"), date = "LAST_14_DAYS", ...)
 {
 	if(!require(RAdwords)) stop("adwords.report requires package RAdwords, use install_github('jburkhardt/RAdwords')")
 	if(!require(data.table)) stop("adwords.report requires package data.table, use install.packages(data.table)")
 
-	query <- RAdwordsPlus::statement(report = name, fields = fields, date = date, ...)
+	query <- RAdwordsPlus::statement(report = report, fields = fields, date = date, ...)
 	response <- lapply(cid, getData, google_auth = auth, statement = query)
 	error <- is.error(response)
 	if(any(error))
