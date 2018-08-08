81f6344d609caf844d452538b8f69e3f7bc1ec9f
nicolas-scott <nicolas.scott@umontreal.ca>
2018-08-08 14:04:57 -0400

    Update to the latest API version and added version argument

--- R/statement.R
+++ R/statement.R
@@ -6,13 +6,14 @@
 #' @param fields Fields to select in the report. This can be a mix of attributes, segments and metrics. Default to "AccountDescriptiveName", "Impressions", "Clicks", "Cost", "Date".
 #' @param date Either a date range (see \url{https://developers.google.com/adwords/api/docs/guides/reporting#date-ranges}) or a vector of length two with the date interval (start date first, end date last). Default to last fourteen days ("LAST_14_DAYS"). Note that this works only if no Date or Week columns are specified (see \url{https://developers.google.com/adwords/api/docs/guides/awql}).
 #' @param where Conditions to use in the statement, like restrictions on a field
+#' @param compress Gzipped data download if TRUE. Default to FALSE.
 #'
 #' @return A character string with the content of the query.
 #' @export
 #'
 #' @examples
 #' statement(report = "ACCOUNT_PERFORMANCE_REPORT", fields = c("AccountDescriptiveName", "Impressions", "Clicks", "Cost", "Date"), date = "LAST_14_DAYS")
-statement <- function(report = "ACCOUNT_PERFORMANCE_REPORT", fields = c("AccountDescriptiveName", "Impressions", "Clicks", "Cost", "Date"), date = "LAST_14_DAYS", where)
+statement <- function(report = "ACCOUNT_PERFORMANCE_REPORT", fields = c("AccountDescriptiveName", "Impressions", "Clicks", "Cost", "Date"), date = "LAST_14_DAYS", where, compress = FALSE)
 {
 	query <- paste0("__rdquery=SELECT+", paste(fields, collapse = ","))
 	query <- paste0(query, "+FROM+", report)
@@ -37,5 +38,6 @@ statement <- function(report = "ACCOUNT_PERFORMANCE_REPORT", fields = c("Account
 
 	query <- paste0(query, "&__fmt=CSV")
 	attr(query, "reportType") <- report
+	attr(query, "compressed") <- compress
 	query
 }
